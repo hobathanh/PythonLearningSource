558d1f103d72136b07b4669e98ea22061f11c1c9
HO BA THANH <hobathanh201@gmail.com>
2022-08-25 17:27:34 +0700

    done Day2

---
+++ Day2/calculator.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip calculator.")
+total = float(input('What was the total bill? '))
+tip = int(input('what precentage tip would you like to give? 10,12,15 ? '))
+person = int(input('How many ? '))
+total_end = round(total*(tip/100+1) / person,2)
+print('Total person each: $',total_end)
